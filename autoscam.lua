e81bd39cde31ae65ca9e29987b832880e4340b1e
hazel-solarisproject <jynksblockout@gmail.com>
2025-12-28 19:32:03 +0200

    Update autoscam.lua

--- autoscam.lua
+++ autoscam.lua
@@ -1,1 +1,1 @@
-Game.Players.LocalPlayer:Kick("The script you just executed is designed to scam you. Nothings been lost. Dont run random scripts!")
+Game.Players.LocalPlayer:Kick("The script you just executed is designed to seal your brainrots. Nothings been lost. Dont run random scripts!")
